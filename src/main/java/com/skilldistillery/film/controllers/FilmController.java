5489c7d1300e63a05de6e24caecb7e8107e8f4a9
Tom Stagnaro <tomstagnaro1@gmail.com>
2021-07-03 13:04:04 -0700

    added searchByKeyword

--- src/main/java/com/skilldistillery/film/controllers/FilmController.java
+++ src/main/java/com/skilldistillery/film/controllers/FilmController.java
@@ -1,5 +1,8 @@
 package com.skilldistillery.film.controllers;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -22,17 +25,20 @@ public class FilmController {
 	
 	@RequestMapping(path = "searchByIdInput.do", params = "filmId", method = RequestMethod.GET)
 	public ModelAndView searchById(int filmId) {
+		List<Film> filmList = new ArrayList<>();
 		ModelAndView mv = new ModelAndView();
-		Film filmResult = dao.findFilmById(filmId);
-		mv.addObject("film", filmResult);
-		mv.setViewName("WEB-INF/home.jsp");
+		filmList.add(dao.findFilmById(filmId));
+		mv.addObject("films", filmList);
+		mv.setViewName("WEB-INF/result.jsp");
 		return mv;
 	}
 	
-	@RequestMapping(path = "searchByKeywordInput.do")
-	public ModelAndView searchByKeyword() {
+	@RequestMapping(path = "searchByKeywordInput.do", params = "keyword", method = RequestMethod.GET)
+	public ModelAndView searchByKeyword(String keyWord) {
 		ModelAndView mv = new ModelAndView();
-		mv.setViewName("WEB-INF/home.jsp");
+		List<Film> filmsByKeword = dao.findFilmsByKeywordInTitleOrDesc(keyWord);
+		mv.addObject("films", filmsByKeword);
+		mv.setViewName("WEB-INF/result.jsp");
 		return mv;
 	}
 	
